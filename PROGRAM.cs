f1b939a1bcc7d66cf2b4cea2e1f077273726bd8f
Dmitry <127277183+Dmitry73109@users.noreply.github.com>
2023-03-09 02:03:09 +0200

    Add menu base

---
+++ PROGRAM.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+    static void Main() {
+        string Answer;
+        //вывести меню в отдельную функцию и вызывать в самом начале
+       Console.WriteLine("Write first letter of the option");
+       Console.WriteLine("New game");
+       Console.WriteLine("Saves");
+       Answer = Console.ReadLine();
+        switch (Answer){
+            case "n":
+                //start the game
+                Console.WriteLine("New game");
+                break;
+            case "s":
+                //load save
+                Console.WriteLine("Saves");
+                break;
+            case "exit":
+                //close the game
+                break;
+            }
+        }
+        
+    }
